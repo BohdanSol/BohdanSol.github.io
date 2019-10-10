25e89e9874aae3f4a00d58751c170285a5c24155
Bohdan Solovey <hondadiozx72@gmail.com>
2019-10-10 17:40:41 +0300

    addons
    added punkt 9 LP + pages

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 new fullpage('#fullpage', {
 	autoScrolling: true,
-	sectionsColor: [ '#707071', '#F49442', '#90C13E', '#4F97D1', '#B6539F' ],
+	sectionsColor: [ '#707071', '#F49442', '#90C13E', '#4F97D1', '#B6539F', '#f2f2f2' ],
 	navigation: true
 });
